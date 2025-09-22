f56b8877e5eeb323b51605d0612ea6502cde5229
yamini <yamini.com>
2025-09-22 17:12:36 +0530

    variable

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 echo "ramesh::hi"
 echo "suresh::hello"
---
+++ 03-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+person1=tom
+person2=jom
+
+echo "$person1::hi $person2,how are you?"
+echo "$person2::hello $person1,i am fine."
